7af8e7561c40359d4405d0076e64bc11ab8305e8
crabonfire <kepitingkebakaran@gmail.com>
2025-05-13 07:07:07 +0000

    submit

---
+++ assignment/greeting.py
@@ -0,0 +1,10 @@
+def greet(name):
+    print(f"Wassup {name} my bro! Welcome to my crib.")
+
+#this is where you ask for the players name 
+player1= input("What's the first player's name?")
+player2= input("What's the second player's name?")
+
+#then you say hello
+greet(player1)
+greet(player2)
